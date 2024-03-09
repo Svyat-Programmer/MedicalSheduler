13d6724db631cddd6f8a20c10fb447691e9d0832
svyat <svyatoslav.irishin@gmail.com>
2024-03-09 11:06:10 +0200

    Done v 1.2+CustomScheduler+Readme+Jar

---
+++ src/test/java/Medical/SchedulerServiceTests.java
@@ -0,0 +1,46 @@
+package Medical;
+
+import org.example.scheduler.MedicalSchedulerApplication;
+import org.example.scheduler.entites.TaskStatus;
+import org.example.scheduler.entites.TreatmentAction;
+import org.example.scheduler.entites.TreatmentTaskEntity;
+import org.example.scheduler.repo.TreatmentPlanRepo;
+import org.example.scheduler.repo.TreatmentTaskRepo;
+import org.example.scheduler.service.SchedulerService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+
+@SpringBootTest(classes = MedicalSchedulerApplication.class)
+    public class SchedulerServiceTests {
+
+        @Autowired
+        private SchedulerService schedulerService;
+
+        @Autowired
+        private TreatmentPlanRepo treatmentPlanRepo;
+
+        @Autowired
+        private TreatmentTaskRepo treatmentTaskRepo;
+
+
+        @Test
+        public void generateTreatmentTasks_ShouldCreateTasksWithCorrectDetails() {
+            schedulerService.generateTreatmentTasks();
+            List<TreatmentTaskEntity> generatedTasks = treatmentTaskRepo.findAll();
+            assertEquals(TreatmentAction.ACTION_A, generatedTasks.get(0).getTreatmentAction());
+            assertEquals("Svyat", generatedTasks.get(0).getSubjectPatient());
+            assertEquals(TaskStatus.ACTIVE, generatedTasks.get(0).getStatus());
+            assertEquals(LocalDateTime.of(2024,3,15,8,00),generatedTasks.get(0).getStartTime());
+        }
+    }
+
+
+
